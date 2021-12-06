abfd5cea8ea57ae00ac7f02f95474b871f4a8383
A Shah <ameeno@users.noreply.github.com>
2021-12-06 13:50:12 +0000

    updating script

--- update.sh
+++ update.sh
@@ -3,17 +3,16 @@
 kubectl get crd -o yaml > crds.yaml
 
 wget https://raw.githubusercontent.com/yannh/kubeconform/master/scripts/openapi2jsonschema.py 
-
+pip freeze openapi2jsonschema.py  > requirements.txt
+pip install -r requirements.txt
 rm -rf schemas
 mkdir schemas
 cd schemas
-
 python3 ../openapi2jsonschema.py ../crds.yaml
-
 python3 ../openapi2jsonschema.py https://doc.crds.dev/raw/github.com/jaegertracing/jaeger-operator
 
 cd ..
 
 rm crds.yaml
 rm openapi2jsonschema.py
-
+rm requirements.txt
